5e6784e959d27218b8353156b4bb9757f62d7908
abhed agarwal <abhed.agl@gmail.com>
2025-06-28 12:48:20 +0530

    debug

--- walmart-mock/backend/server.js
+++ walmart-mock/backend/server.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 // MongoDB connection
-mongoose.connect("mongodb://127.0.0.1:27017/shopsmart", {
+mongoose.connect("mongodb://127.0.0.1:27017/walmart",{
   useNewUrlParser: true,
   useUnifiedTopology: true
 }).then(() => console.log("✅ MongoDB connected"))
